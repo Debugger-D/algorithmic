af8f4318d643794ae6de15260107de8507602815
jim.jia <zhenshanjia@foxmail.com>
2019-07-19 20:39:57 +0800

    feat: add some files

---
+++ String/1.reverseString.js
@@ -0,0 +1,13 @@
+/**f
+ * 反转字符串
+ * 输入：["h","e","l","l","o"]
+ * 输出：["o","l","l","e","h"]
+  */
+
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var reverseString = function(s) {
+  s.reverse()
+};
